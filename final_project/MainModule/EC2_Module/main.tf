07d065772a0cb8e2bc998d08086ea784ac7c797a
nigorasultonov <152802393+nigorasultonov@users.noreply.github.com>
2024-03-03 13:32:51 -0500

    Create main.tf

---
+++ final_project/MainModule/EC2_Module/main.tf
@@ -0,0 +1,32 @@
+# ec2_module/main.tf
+
+resource "aws_instance" "ec2_instance" {
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  count = length(var.instance_name)
+  ami           = "ami-088495de30f0655c4"     # Replace with appropriate ami-ID
+  instance_type = lookup(var.instance_type, var.instance_name[count.index])
+  key_name      = aws_key_pair.pub_key.key_name
+
+# User data script for instance initialization
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y               # Update the system
+              git clone https://github.com/nigorasultonov/final-project-resources-DevOps2022.git       # Clone the project repository. I'll update the url once I create private repo with terraform
+  
+  tags = merge(
+    {
+      Name        = var.instance_name[count.index]
+      Team        = "TuranCyberHub"
+      CreatedTime = "02.28.2024"
+    },
+    var.tags
+  )
+}
+resource "aws_key_pair" "pub_key" {
+  key_name   = "ssh-key"
+  public_key = var.ssh_pub_key
+}
+
+
+
