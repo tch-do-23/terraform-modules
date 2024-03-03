6a18b180b1ecf611317d8475b75eed904fa0ec64
tch-do-23 <161963143+tch-do-23@users.noreply.github.com>
2024-03-03 16:32:26 -0500

    Update git_variables.tf

--- final_project/git_hub/git_variables.tf
+++ final_project/git_hub/git_variables.tf
@@ -1,4 +1,4 @@
 variable "github_token" {
   type = string
-  default = "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"
+  default = "your_github_token"
 }
