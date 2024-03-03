a03a6d8e4cf8d2e54e80a18464374c15c3fa7404
tch-do-23 <161963143+tch-do-23@users.noreply.github.com>
2024-03-03 16:26:29 -0500

    Update main.tf

--- final_project/MainModule/main.tf
+++ final_project/MainModule/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   count = length(var.instance_name)
-  ami           = "ami-088495de30f0655c4"     # Replace with appropriate ami-ID
+  ami           = "i-0a6ed7e0c66f224b4"     # Replace with appropriate ami-ID
   instance_type = lookup(var.instance_type, var.instance_name[count.index])
   key_name      = aws_key_pair.pub_key.key_name
 
@@ -12,7 +12,7 @@ resource "aws_instance" "ec2_instance" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y               # Update the system
-              git clone https://github.com/nigorasultonov/final-project-resources-DevOps2022.git       # Clone the project repository. I'll update the url once I create private repo with terraform
+              git clone        # Clone the project repository. I'll update the url once I create private repo with terraform
   
   tags = merge(
     {
